75d015f25644a9d02620b34d11e31c772531e610
keshav4r3 <keshav4r3@gmail.com>
2025-06-02 09:32:47 +0530

    positioning of cors

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { dataBaseConnection } = require('./config/databaseConnection')
-const router = require('./route/router')
+const { dataBaseConnection } = require('./config/databaseConnection');
+const router = require('./route/router');
 
-
-app.use(express.json());
-dataBaseConnection();
-app.use(router);
 app.use(cors({
     origin: 'http://localhost:5173',
-    withCredentials: true
+    credentials: true
 }));
 
-const port = process.env.PORT || 5001
+app.use(express.json());
+
+dataBaseConnection();
+app.use(router);
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
-    console.log("server started at port number ", port)
-})
+    console.log("Server started at port number", port);
+});
