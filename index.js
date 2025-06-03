6cad12579ee69db288f85c77e29e2e1cead4bb15
keshav4r3 <keshav4r3@gmail.com>
2025-06-03 11:24:17 +0530

    index.js updated

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ require('dotenv').config();
 const { dataBaseConnection } = require('./config/databaseConnection');
 const router = require('./route/router');
 const cookieParser = require('cookie-parser');
-app.use(cors({
-    origin: 'https://portfolio-frontend-lac-theta.vercel.app',
-    credentials: true
-}));
+
+const corsOptions = {
+  origin: 'https://portfolio-frontend-lac-theta.vercel.app',  // Vercel frontend URL
+//   origin: 'http://localhost:5173',
+  credentials: true,  // Allow cookies to be sent and received
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+
 
 app.use(cookieParser());
 app.use(express.json());
