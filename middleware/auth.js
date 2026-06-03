be945832f3f23d59f053cb3ae3a64eb38b625f7c
keshav412003 <keshav412003@gmail.com>
2026-06-03 21:24:14 +0530

    fix: MAIL_USER -> SMTP_USER in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ exports.isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded || decoded.email != process.env.MAIL_USER) {
+        if (!decoded || decoded.email != process.env.SMTP_USER) {
             return res.status(401).json({
                 success: false,
                 message: "invalid token"
